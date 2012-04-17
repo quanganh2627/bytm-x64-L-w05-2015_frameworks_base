da3a7ffd01ed09d85136db65e2b5c082dd5a5fe8
shi yang <yang.a.shi@intel.com>
2012-04-17 11:10:03 +0800

    change the implementation of get image file size
    BZ: 30940
    
    Because File can't get permission to get file size of screenshot image.
    So use InputStream to get file size correctly of screenshot image.
    
    Change-Id: Ic2e6b8bdf5e776256a744a6602d1953d15bc97b3
    Signed-off-by: Bo Huang <bo.b.huang@intel.com>
    Signed-off-by: shi yang <yang.a.shi@intel.com>
    Reviewed-on: http://android.intel.com:8080/79214
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Du, Chenyang <chenyang.du@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -56,7 +56,7 @@ import android.view.animation.Interpolator;
 import android.widget.ImageView;
 
 import com.android.systemui.R;
-
+import java.io.InputStream;
 import java.io.File;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
@@ -204,8 +204,11 @@ class SaveImageInBackgroundTask extends AsyncTask<SaveImageInBackgroundData, Voi
 
             // update file size in the database
             values.clear();
-            values.put(MediaStore.Images.ImageColumns.SIZE, new File(mImageFilePath).length());
+            InputStream in = resolver.openInputStream(uri);
+            int size = in.available();
+            values.put(MediaStore.Images.ImageColumns.SIZE, size);
             resolver.update(uri, values, null, null);
+            in.close();
 
             params[0].imageUri = uri;
             params[0].result = 0;
