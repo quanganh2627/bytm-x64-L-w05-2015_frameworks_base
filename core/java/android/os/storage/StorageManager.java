5fdbbefbf06e0f6be64b91ce67bc5fbb0f33fe98
Chuanxiao Dong <chuanxiao.dong@intel.com>
2012-04-27 21:06:00 +0800

    StorageManager: only register MountService listener when needed
    BZ: 30943
    
    When other service get StorageManager, StorageManager will register a
    listener in MountService, which won't be unregistered.
    
    So change the policy to be:
    1. when other service needs to register listener in StorageManager,
    StorageManager will register listener in MountService.
    2. When other service needs to unreg0ster listener in StorageManager,
    if there is no more other listeners in StorageManager, StorageManager
    will unregister listener in MountService
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    
    Conflicts:
    
    	core/java/android/os/storage/StorageManager.java
    
    Change-Id: Ia085500174c591adce56d0e055140570c316e47b
    Signed-off-by: bdeng3X <bingx.deng@intel.com>
    Reviewed-on: http://android.intel.com:8080/75938
    Reviewed-by: lab_cactus <lab_cactus@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- core/java/android/os/storage/StorageManager.java
+++ core/java/android/os/storage/StorageManager.java
@@ -331,7 +331,7 @@ public class StorageManager
         }
 
         synchronized (mListeners) {
-            if (mBinderListener == null ) {
+            if (mBinderListener == null && mMountService != null) {
                 try {
                     mBinderListener = new MountServiceBinderListener();
                     mMountService.registerListener(mBinderListener);
@@ -365,7 +365,7 @@ public class StorageManager
                     break;
                 }
             }
-            if (mListeners.size() == 0 && mBinderListener != null) {
+            if (mListeners.size() == 0 && mBinderListener !=null && mMountService != null) {
                 try {
                     mMountService.unregisterListener(mBinderListener);
                 } catch (RemoteException rex) {
