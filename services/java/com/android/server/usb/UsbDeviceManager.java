5e5ca514edc9eaf29668f4b968fe58c99041d13f
Wu, Hao <hao.wu@intel.com>
2012-07-23 17:17:58 +0800

    [PORT FROM MAIN]UsbDeviceManager: turn on Screen to notify user USB connection event
    BZ: 126631
    
    In some devices, due to no USB charging, so screen will not be on
    when we plug usb to PC. This patch adds this support, by holding a
    wakelock for a while.
    
    Change-Id: I07bcf67453e8d80e4010a29bf9df31f4be8c0e51
    Orig-Change-Id: I20edc5fecb51df559dd77218601d728cbe15f64f
    Signed-off-by: Wu, Hao <hao.wu@intel.com>
    Reviewed-on: http://android.intel.com:8080/122877
    Reviewed-by: Du, ChangbinX <changbinx.du@intel.com>
    Tested-by: Wang, Ran <ran.wang@intel.com>
    Reviewed-by: Wang, Yu Y <yu.y.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -36,6 +36,8 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.os.ParcelFileDescriptor;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.os.Process;
 import android.os.SystemClock;
 import android.os.SystemProperties;
@@ -103,6 +105,7 @@ public class UsbDeviceManager {
 
     private static final String BOOT_MODE_PROPERTY = "ro.bootmode";
 
+    private Handler mLockHandler;
     private UsbHandler mHandler;
     private boolean mBootCompleted;
 
@@ -110,8 +113,11 @@ public class UsbDeviceManager {
 
     private final Context mContext;
     private final ContentResolver mContentResolver;
+
     @GuardedBy("mLock")
     private UsbSettingsManager mCurrentSettings;
+    private final PowerManager mPowerManager;
+    private final PowerManager.WakeLock mWakeLock;
     private NotificationManager mNotificationManager;
     private final boolean mHasUsbAccessory;
     private boolean mUseUsbNotification;
@@ -162,9 +168,14 @@ public class UsbDeviceManager {
         mContentResolver = context.getContentResolver();
         PackageManager pm = mContext.getPackageManager();
         mHasUsbAccessory = pm.hasSystemFeature(PackageManager.FEATURE_USB_ACCESSORY);
+        mPowerManager = (PowerManager)mContext.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "usb_lock");
+        mWakeLock.setReferenceCounted(false);
         initRndisAddress();
 
         readOemUsbOverrideConfig();
+
+        mLockHandler = new Handler();
 
         // create a thread for our Handler
         HandlerThread thread = new HandlerThread("UsbDeviceManager",
@@ -648,6 +659,11 @@ public class UsbDeviceManager {
                         if (mConnected == mConfigured)
                             updateAudioSourceFunction();
                     }
+                    if (mConnected && mConfigured) {
+                        mLockHandler.removeCallbacks(usbConLCDTask);
+                        mWakeLock.acquire();
+                        mLockHandler.postDelayed(usbConLCDTask, 10000);
+                    }
                     break;
                 case MSG_ENABLE_ADB:
                     setAdbEnabled(msg.arg1 == 1);
@@ -955,6 +971,13 @@ public class UsbDeviceManager {
                         + "UsbDebuggingManager not enabled");
         }
     }
+
+    private Runnable usbConLCDTask = new Runnable() {
+        public void run() {
+            if (mWakeLock.isHeld())
+                mWakeLock.release();
+        }
+    };
 
     public void dump(FileDescriptor fd, PrintWriter pw) {
         if (mHandler != null) {
