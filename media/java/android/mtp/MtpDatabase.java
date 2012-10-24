3522eb1583f0b08795f5ea4428f1a0f55e153656
Bo Huang <bo.b.huang@intel.com>
2012-10-24 12:06:54 +0800

    [PORT FROM MAI]Mtp: MtpDatabase TOMBSTONE continue happen when do usb otg plug/unplug test.
    BZ: 60891
    
    Add one JNI interface in MtpDatabase to delete global reference
    in order to avoid GREF issue.
    
    Change-Id: I2289914b26acc7b35d9620cc07969290191430c5
    Signed-off-by: Bo Huang <bo.b.huang@intel.com>
    Reviewed-on: http://android.intel.com:8080/76536
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Meng, Zhe <zhe.meng@intel.com>
    Tested-by: Meng, Zhe <zhe.meng@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- media/java/android/mtp/MtpDatabase.java
+++ media/java/android/mtp/MtpDatabase.java
@@ -170,13 +170,13 @@ public class MtpDatabase {
         initDeviceProperties(context);
     }
 
+    public void release() {
+            native_release();
+    }
+
     @Override
-    protected void finalize() throws Throwable {
-        try {
+    protected void finalize() {
             native_finalize();
-        } finally {
-            super.finalize();
-        }
     }
 
     public void addStorage(MtpStorage storage) {
@@ -1027,4 +1027,5 @@ public class MtpDatabase {
 
     private native final void native_setup();
     private native final void native_finalize();
+    private native final void native_release();
 }
