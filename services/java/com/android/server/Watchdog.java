65b0137379e8aaba1cb1c238b9fe1c41f146c1c9
Jean-Christophe PINCE <jean-christophe.pince@intel.com>
2012-11-06 17:18:05 +0100

    [PORT FROM MAIN] Add Watchdog timing information
    BZ: 66696
    
    Log timing information when the Watchdog's timeout
    elapses so we can understand which monitor took
    too long; this is especially usefull when mCurrentMonitor
    is null but also when the monitors take too long but
    return eventually.
    
    Conflicts:
    	services/java/com/android/server/Watchdog.java
    
    Category: aosp improvement
    Domain: AOSP-Framework-OS
    Severity: medium
    Origin: internal
    Upstream-Candidate: yes
    
    Change-Id: I98348508f9a500dc77cc2a1f7977249ccf434443
    Orig-Change-Id: Ifcaf3f7201d829922f474c4e68bdf66b98392ae6
    Signed-off-by: Jean-Christophe PINCE <jean-christophe.pince@intel.com>

--- services/java/com/android/server/Watchdog.java
+++ services/java/com/android/server/Watchdog.java
@@ -72,6 +72,8 @@ public class Watchdog extends Thread {
     static final int REBOOT_DEFAULT_START_TIME = 3*60*60;                  // 3:00am
     static final int REBOOT_DEFAULT_WINDOW = 60*60;                        // within 1 hour
 
+    static final int NB_TIMINGS = 5;
+
     static final String REBOOT_ACTION = "com.android.service.Watchdog.REBOOT";
 
     static final String[] NATIVE_STACKS_OF_INTEREST = new String[] {
@@ -92,6 +94,8 @@ public class Watchdog extends Thread {
     ActivityManagerService mActivity;
     boolean mCompleted;
     Monitor mCurrentMonitor;
+    long mTimings[][];
+    int mCurrentTiming = 0;
 
     int mPhonePid;
     IActivityController mController;
@@ -138,12 +142,18 @@ public class Watchdog extends Thread {
                     }
 
                     final int size = mMonitors.size();
+                    long start, stop = SystemClock.uptimeMillis();
                     for (int i = 0 ; i < size ; i++) {
                         synchronized (Watchdog.this) {
                             mCurrentMonitor = mMonitors.get(i);
                         }
+                        start = stop;
                         mCurrentMonitor.monitor();
+                        stop = SystemClock.uptimeMillis();
+                        mTimings[i][mCurrentTiming] = (stop - start);
                     }
+                    mCurrentTiming++;
+                    if (mCurrentTiming >= NB_TIMINGS) mCurrentTiming = 0;
 
                     synchronized (Watchdog.this) {
                         mCompleted = true;
@@ -402,6 +412,7 @@ public class Watchdog extends Thread {
     @Override
     public void run() {
         boolean waitedHalf = false;
+        mTimings = new long[mMonitors.size()][NB_TIMINGS];
         while (true) {
             mCompleted = false;
             mHandler.sendEmptyMessage(MONITOR);
@@ -452,6 +463,18 @@ public class Watchdog extends Thread {
             // First collect stack traces from all threads of the system process.
             // Then kill this process so that the system will restart.
             EventLog.writeEvent(EventLogTags.WATCHDOG, name);
+
+            // Prints out the timings of the last NB_TIMINGS checks so that
+            // it can help understanding which component took too much time
+            // but finally returned
+            for (int i = 0 ; i < mMonitors.size() ; i++) {
+                Monitor monitor = mMonitors.get(i);
+                String sMonitor = "Unkown monitor: ";
+                if (monitor != null) sMonitor = monitor.getClass().getName() + ": ";
+                for (int j = 0 ; j < NB_TIMINGS ; j++)
+                    sMonitor += "" + mTimings[i][j] + "ms ";
+                Log.i(TAG, sMonitor);
+            }
 
             ArrayList<Integer> pids = new ArrayList<Integer>();
             pids.add(Process.myPid());
