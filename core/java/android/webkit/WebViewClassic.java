d3670ee867b4633707a7e650b41aa0c1c282b090
binx.xu <binx.xu@intel.com>
2012-11-23 14:23:34 +0000

    [PORT FROM MAIN]crashed JAVACRASH on 'com.google.android.apps.docs'
    BZ: 68896
    
    other app call webviewcore, but it is not initialized, so
    check the null pointer when run in webview.
    Signed-off-by: binx.xu <binx.xu@intel.com>
    
    Change-Id: Ic08b6666c0e35360ef4501ab54154153edc4a155
    Orig-Change-Id: I4bc4be1136804ffce7190db94d9b5692e74b742e
    Reviewed-on: http://android.intel.com:8080/122049
    Reviewed-by: Xu, BinX <binx.xu@intel.com>
    Reviewed-by: Qin, Xiaokang <xiaokang.qin@intel.com>
    Tested-by: Qin, Xiaokang <xiaokang.qin@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- core/java/android/webkit/WebViewClassic.java
+++ core/java/android/webkit/WebViewClassic.java
@@ -2570,6 +2570,9 @@ public final class WebViewClassic implements WebViewProvider, WebViewProvider.Sc
     }
 
     private void loadUrlImpl(String url, Map<String, String> extraHeaders) {
+        if (mWebViewCore == null) {
+            return;
+        }
         switchOutDrawHistory();
         WebViewCore.GetUrlData arg = new WebViewCore.GetUrlData();
         arg.mUrl = url;
