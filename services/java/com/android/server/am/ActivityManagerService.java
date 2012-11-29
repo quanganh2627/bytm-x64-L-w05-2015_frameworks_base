ddc1072b89df397dd70918af621cb57bb262b0c3
bdeng3X <bingx.deng@intel.com>
2012-11-29 14:14:47 +0800

    [PORT FROM MAIN] Catch the NameNotFoundException and return to avoid null pointer when updating package in AMS.
    BZ: 63612
    
    Catch the NameNotFoundException and return to avoid null pointer when updating package in AMS.
    Signed-off-by: bdeng3X <bingx.deng@intel.com>
    
    Change-Id: Ibff00039c94fcc6b545cb21d363fbad591ed2103
    Signed-off-by: bdeng3X <bingx.deng@intel.com>
    Reviewed-on: http://android.intel.com:8080/80439
    Reviewed-by: Zhang, Hongyu <hongyu.zhang@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -2087,6 +2087,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                     }
                 } catch (PackageManager.NameNotFoundException e) {
                     Slog.w(TAG, "Unable to retrieve gids", e);
+                    return;
                 }
 
                 /*
