86f6629ae01a3ca49dfe2d4a68e78033fd3c1b20
Sebastian Niciarz <sebastian.niciarz@intel.com>
2012-12-09 14:08:43 +0200

    base: Integration of SmartCard API V2.3.1
    BZ: 21770
    
    DO NOT UPSTREAM
    
    smartcard-api.patch and uicc.patch from:
    https://code.google.com/p/seek-for-android/downloads/list
    
    Description:
    VersionName: 2.3.1-rc1
    VersionCode: 0 (Pre-Release)
    
    Features:
    - ICS support (Android-4.0.3)
    - compatible with SCAPI-2.3.0
    - Compliant to SIMalliance Open Mobile API Specification V2.02
    - Support of SIM, embedded SE & ASSD terminal
    - Removed version 1 interface android.smartcard
    
    Known limitations:
    - logical channels based access to UICC not supported
    
    Change-Id: I4e53f7b885e40355ea107ef0798eb05fd1c2e837
    Origin-Change-Id: I716710df2ddaa6ee7fd4e21b707cb41f6aa5b1d9
    Signed-off-by: Sebastian Niciarz <sebastian.niciarz@intel.com>
    Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
    Reviewed-on: http://android.intel.com:8080/80070
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- telephony/java/com/android/internal/telephony/RILConstants.java
+++ telephony/java/com/android/internal/telephony/RILConstants.java
@@ -56,6 +56,9 @@ public interface RILConstants {
                                                  to wrong SIM/ME and no
                                                  retries needed */
     int NETWORK_PUK_REQUIRED = 16;            /* Network Personalization PUK required */
+    int MISSING_RESOURCE = 17;
+    int NO_SUCH_ELEMENT = 18;
+    int INVALID_PARAMETER = 19;
 
     /* NETWORK_MODE_* See ril.h RIL_REQUEST_SET_PREFERRED_NETWORK_TYPE */
     int NETWORK_MODE_WCDMA_PREF     = 0; /* GSM/WCDMA (WCDMA preferred) */
@@ -264,6 +267,10 @@ cat include/telephony/ril.h | \
     int RIL_REQUEST_ACKNOWLEDGE_INCOMING_GSM_SMS_WITH_PDU = 106;
     int RIL_REQUEST_STK_SEND_ENVELOPE_WITH_STATUS = 107;
     int RIL_REQUEST_VOICE_RADIO_TECH = 108;
+    int RIL_REQUEST_SIM_TRANSMIT_BASIC = 109;
+    int RIL_REQUEST_SIM_OPEN_CHANNEL = 110;
+    int RIL_REQUEST_SIM_CLOSE_CHANNEL = 111;
+    int RIL_REQUEST_SIM_TRANSMIT_CHANNEL = 112;
     int RIL_UNSOL_RESPONSE_BASE = 1000;
     int RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED = 1000;
     int RIL_UNSOL_RESPONSE_CALL_STATE_CHANGED = 1001;
