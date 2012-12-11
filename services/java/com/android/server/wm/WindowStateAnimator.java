4ce54753efd77239199eeebe5a173dacd4c89802
Jun Wu <junx.wu@intel.com>
2012-12-11 14:51:02 +0800

    Pop-up option menu can't be taped when unlock the screen.
    BZ: 68374
    
    RootCause:
    There are 3 window(Subsettings,Add Window, PopupWindow) at Subsettings activity.
    After resume processing. The subsettings relayout process just change
    the focus to "Add Window" not PopupWindow.
    The PopupWindow's AbsListView accept the input event, but not do PerformClick because
    The AbsListView is not at focus window.
    
    Add the flag to let the WMS know that WMS should updateFocusWindow
    after the PopupWindow's animation finish.
    
    Change-Id: Ic3caeff953223dfa0f44c6e87a880b34817fbb6a
    Original-change-Id: I8b4777dec1e639b279ca79f80a99a6971548a72d
    Signed-off-by: Ding,ErjunX <erjunx.ding@intel.com>
    Reviewed-on: http://android.intel.com:8080/80519
    Reviewed-by: Yin, Fengwei <fengwei.yin@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/com/android/server/wm/WindowStateAnimator.java
+++ services/java/com/android/server/wm/WindowStateAnimator.java
@@ -1419,6 +1419,7 @@ class WindowStateAnimator {
                         // loop, this will cause it to restart with a new
                         // layout.
                         c.mDisplayContent.layoutNeeded = true;
+                        mService.mFocusMayChange = true;
                     }
                 }
             }
