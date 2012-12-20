aa520ca7b597fcec0c0446fc3afb4222dfcb2f02
David Castelain <davidx.castelain@intel.com>
2012-12-20 13:41:05 +0100

    [PORT FROM MAIN] Do not start longLongpress timer on ENDCALL handling
    BZ: 60053
    
    longLongPress timer must not be started on a KEYCODE_ENDCALL handling.
    Adding boolean allowing or not to trigger longLongPress event.
    
    Change-Id: I9c365b405f06dfed549196a79ded857741069de4
    Orig-Change-Id: Ia36e5465bd0531c375069740b17ead8356b9b5b9
    Category: feature differentiation
    Domain: AOSP-Framework-CoreService
    Origin: internal
    Upstream-Candidate: no
    Signed-off-by: Frederic Despalles <frederic.fespalles@intel.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -667,7 +667,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    private void interceptPowerKeyDown(boolean handled) {
+    private void interceptPowerKeyDown(boolean handled, boolean longLongPressCandidate) {
         mPowerKeyHandled = handled;
         if (!handled) {
             mPowerKeyTimeout = ViewConfiguration.getGlobalActionKeyTimeout();
@@ -675,10 +675,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // Ask mPowerLongPress to be run when specified amount of time elapses.
             mHandler.postDelayed(mPowerLongPress, mPowerKeyTimeout);
         } else {
-            mPowerKeyTimeout = ViewConfiguration.getGlobalActionKeyShutdownTimeout() + ViewConfiguration.getGlobalActionKeyTimeout();
-            Log.i(TAG, String.format("interceptPowerKeyDown: key down event: handled: set timeout to %d ms", mPowerKeyTimeout));
-            // Ask mPowerLongLongPress to be run when specified amount of time elapses.
-            mHandler.postDelayed(mPowerLongLongPress, mPowerKeyTimeout);
+            // only for calls from POWER BUTTON handling can start longLongPress timer
+            if (longLongPressCandidate) {
+                mPowerKeyTimeout = ViewConfiguration.getGlobalActionKeyShutdownTimeout() + ViewConfiguration.getGlobalActionKeyTimeout();
+                Log.i(TAG, String.format("interceptPowerKeyDown: key down event: handled: set timeout to %d ms", mPowerKeyTimeout));
+                // Ask mPowerLongLongPress to be run when specified amount of time elapses.
+                mHandler.postDelayed(mPowerLongLongPress, mPowerKeyTimeout);
+            }
         }
     }
 
@@ -3992,7 +3995,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             Log.w(TAG, "ITelephony threw RemoteException", ex);
                         }
                     }
-                    interceptPowerKeyDown(!isScreenOn || hungUp);
+                    interceptPowerKeyDown(!isScreenOn || hungUp, false);
                 } else {
                     if (interceptPowerKeyUp(canceled)) {
                         if ((mEndcallBehavior
@@ -4042,7 +4045,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         }
                     }
                     interceptPowerKeyDown(!isScreenOn || hungUp
-                            || mVolumeDownKeyTriggered || mVolumeUpKeyTriggered);
+                            || mVolumeDownKeyTriggered || mVolumeUpKeyTriggered, true);
                 } else {
                     mPowerKeyTriggered = false;
                     cancelPendingScreenshotChordAction();
