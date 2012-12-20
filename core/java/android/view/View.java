b5e273a5cd7a9bb96f5e4455ad0d092b7abe1dad
Wang, Zhifeng <zhifeng.wang@intel.com>
2012-12-20 08:56:28 +0800

    View: correct view's requested visible rectangle calculation
    BZ: 76135
    
    There is an error in view's requested visible rectangle calculation. In some
    cases, it will cause focused TextView can't show above IME softkeyboard. This
    commit fix the issue according to JB4.1 implementation.
    
    Change-Id: I5bbf1053eec72a55b088ed60deb9d3e27019804a
    Signed-off-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/82831
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -4435,14 +4435,13 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
             }
 
             position.offset(child.mLeft, child.mTop);
+            position.offset(-child.getScrollX(), -child.getScrollY());
 
             if (!(parent instanceof View)) {
                 break;
             }
 
             View parentView = (View) parent;
-
-            position.offset(-parentView.getScrollX(), -parentView.getScrollY());
 
             child = parentView;
             parent = child.getParent();
