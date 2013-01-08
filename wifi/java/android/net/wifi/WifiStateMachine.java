d1adf9014ab684cd35b6a1556ecbb77dc95dbfd7
Frode Isaksen <fisaksex@intel.com>
2013-01-08 14:52:31 +0100

    [PORT FROM MAIN]wifi: filter out IPv6 multicast packets by default
    BZ: 75372
    
    Filter out IPv6 multicast packets by default
    to reduce wakeups from S3 caused by these packets.
    The IPv6 mutlicast packets can safely be filtered out,
    since IPv6 is not working correctly anyway "out of the box".
    
    Change-Id: I1c85b57307464396babb933e0a4d7de894e99764
    Orig-Change-Id: Iaf4e9cd7dedb64cf0d81904fbb82df6757afb041
    Category: device enablement
    Domain: CWS.WIFI-Common
    Origin: internal
    Upstream-Candidate: no,proprietary
    Signed-off-by: Frode Isaksen <fisaksex@intel.com>
    Reviewed-on: http://android.intel.com:8080/125010
    Reviewed-by: Poulain, LoicX <loicx.poulain@intel.com>
    Reviewed-by: Naoumenko, PaulX <paulx.naoumenko@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -1505,14 +1505,14 @@ public class WifiStateMachine extends StateMachine {
     }
 
     /**
-     * Start filtering Multicast v4 packets
+     * Start filtering Multicast v6 packets
      */
     public void startFilteringMulticastV6Packets() {
         sendMessage(CMD_START_PACKET_FILTERING, MULTICAST_V6, 0);
     }
 
     /**
-     * Stop filtering Multicast v4 packets
+     * Stop filtering Multicast v6 packets
      */
     public void stopFilteringMulticastV6Packets() {
         sendMessage(CMD_STOP_PACKET_FILTERING, MULTICAST_V6, 0);
@@ -3033,8 +3033,8 @@ public class WifiStateMachine extends StateMachine {
             /* initialize network state */
             setNetworkDetailedState(DetailedState.DISCONNECTED);
 
-            /* Remove any filtering on Multicast v6 at start */
-            mWifiNative.stopFilteringMulticastV6Packets();
+            /* Start filtering Multicast v6 packets */
+            mWifiNative.startFilteringMulticastV6Packets();
 
             /* Reset Multicast v4 filtering state */
             if (mFilteringMulticastV4Packets.get()) {
