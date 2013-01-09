4839ea690fd9582aa24c6f61b74357a665119cbb
Leo Yan <leo.yan@intel.com>
2013-01-09 08:12:40 +0800

    Display brightness: Change the condition of display auto brightness
    BZ: 76345
    
    On the platform which button LED autobrightness is configured, the
    light sensor will be always enabled when the screen is on, so we
    can't judge the status of display auto brightness just according to
    the light sensor.
    
    Change-Id: I5403a7ae36fc5ed415b0e4637646a64b6b06e559
    Signed-off-by: Leo Yan <leo.yan@intel.com>
    Reviewed-on: http://android.intel.com:8080/85727
    Reviewed-by: Chen, Jason CJ <jason.cj.chen@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -656,7 +656,7 @@ final class DisplayPowerController {
         if (wantScreenOn(mPowerRequest.screenState)) {
             int target;
             boolean slow;
-            if (mScreenAutoBrightness >= 0 && mLightSensorEnabled) {
+            if (mScreenAutoBrightness >= 0 && mAutoBrightnessEnabled) {
                 // Use current auto-brightness value.
                 target = mScreenAutoBrightness;
                 slow = mUsingScreenAutoBrightness;
@@ -886,8 +886,9 @@ final class DisplayPowerController {
 
     private void setLightSensorEnabled(boolean enable, boolean updateAutoBrightness) {
         if (enable) {
-            if (!mLightSensorEnabled) {
+            if (mAutoBrightnessEnabled)
                 updateAutoBrightness = true;
+            if (!mLightSensorEnabled) {
                 mLightSensorEnabled = true;
                 mLightSensorEnableTime = SystemClock.uptimeMillis();
                 mSensorManager.registerListener(mLightSensorListener, mLightSensor,
@@ -924,8 +925,10 @@ final class DisplayPowerController {
     private void handleLightSensorEvent(long time, float lux) {
         mHandler.removeMessages(MSG_LIGHT_SENSOR_DEBOUNCED);
 
-        applyLightSensorMeasurement(time, lux);
-        updateAmbientLux(time);
+        if (mAutoBrightnessEnabled) {
+            applyLightSensorMeasurement(time, lux);
+            updateAmbientLux(time);
+        }
         if (mButtonAutoBrightnessEnabled)
             updateButtonBrightness(lux);
     }
