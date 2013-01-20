d1818d5c3a4356e70d4bda8d084d0cc81645385b
MinYeolLim <min.yeol.lim@intel.com>
2013-01-20 15:02:36 -0600

    Enabling wakelock tracing for power analysis tool like Wuwatch
    BZ: 81457
    
    The user wakelock tracing analysis in Wuwatch tool is broken in Android
    JellyBean MR1 branch due to power framework change. This is the new
    patch for the change.
    
    Change-Id: Idde068c14ecb9f72c770fd82c7c9ea5f2b44281b
    Signed-off-by: MinYeolLim <min.yeol.lim@intel.com>
    Reviewed-on: http://android.intel.com:8080/87650
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Popescu, Catalin <catalin.popescu@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Shen, JianchunX <jianchunx.shen@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -53,6 +53,7 @@ import android.os.SystemClock;
 import android.os.SystemService;
 import android.os.UserHandle;
 import android.os.WorkSource;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.util.EventLog;
 import android.util.Log;
@@ -632,6 +633,13 @@ public final class PowerManagerService extends IPowerManager.Stub
                 mWakeLocks.add(wakeLock);
             }
 
+            // Generate user-level wakelock traces for Wuwatch
+            if (SystemProperties.get("wakelock.trace", "unknown").equals("1")) {
+                Slog.i("WAKELOCK_ACQUIRE", "TIMESTAMP=" + SystemClock.elapsedRealtimeNanos()
+                        + ", TAG=" + tag + ", TYPE=" + wakeLock.getLockLevelString()
+                        + ", COUNT=0" + ", PID=" + pid + ", UID=" + uid);
+            }
+
             applyWakeLockFlagsOnAcquireLocked(wakeLock);
             mDirty |= DIRTY_WAKE_LOCKS;
             updatePowerStateLocked();
@@ -695,6 +703,14 @@ public final class PowerManagerService extends IPowerManager.Stub
             applyWakeLockFlagsOnReleaseLocked(wakeLock);
             mDirty |= DIRTY_WAKE_LOCKS;
             updatePowerStateLocked();
+
+            // Generate user-level wakelock traces for Wuwatch
+            if (SystemProperties.get("wakelock.trace", "unknown").equals("1")) {
+                Slog.i("WAKELOCK_RELEASE", "TIMESTAMP=" + SystemClock.elapsedRealtimeNanos()
+                        + ", TAG=" + wakeLock.mTag + ", TYPE=" + wakeLock.getLockLevelString()
+                        + ", COUNT=0" + ", PID=" + wakeLock.mOwnerPid + ", UID="
+                        + wakeLock.mOwnerUid);
+            }
         }
     }
 
@@ -715,6 +731,14 @@ public final class PowerManagerService extends IPowerManager.Stub
             applyWakeLockFlagsOnReleaseLocked(wakeLock);
             mDirty |= DIRTY_WAKE_LOCKS;
             updatePowerStateLocked();
+
+            // Generate user-level wakelock traces for Wuwatch
+            if (SystemProperties.get("wakelock.trace", "unknown").equals("1")) {
+                Slog.i("WAKELOCK_RELEASE", "TIMESTAMP=" + SystemClock.elapsedRealtimeNanos()
+                        + ", TAG=" + wakeLock.mTag + ", TYPE=" + wakeLock.getLockLevelString()
+                        + ", COUNT=0" + ", PID=" + wakeLock.mOwnerPid + ", UID="
+                        + wakeLock.mOwnerUid);
+            }
         }
     }
 
