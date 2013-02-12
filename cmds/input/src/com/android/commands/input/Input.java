ae83755f88c74e03248199fded507e6e5b34dea5
Clement Calmels <clement.calmels@intel.com>
2013-02-12 09:57:44 +0100

    [PORT FROM MAIN] Check the InputManager service availability
    BZ: 86288
    
    If the InputManager service is not available during an 'input'
    command, the command will crash with a
    java.lang.NullPointerException. As seen in the bz: 74972, it can be
    confusing for bug analyses as the Exception will also dump JavaBinder
    and ServiceManager traces.
    This patch adds some checks in order to deal with such cases. The
    'input' command will now exit cleanly and report an 'InputManager
    service is not available' error to the user.
    
    Category: aosp improvement
    Domain: AOSP-Framework-OS
    Severity: medium
    Origin: internal
    Upstream-Candidate: yes
    
    Change-Id: Ia85a1e502c280288a8089f4748bcdb0c186e9f60
    Orig-Change-Id: I550217f8c2f4f8be55ddd895c380d016355fce9e
    Signed-off-by: Clement Calmels <clement.calmels@intel.com>

--- cmds/input/src/com/android/commands/input/Input.java
+++ cmds/input/src/com/android/commands/input/Input.java
@@ -212,8 +212,13 @@ public class Input {
 
     private void injectKeyEvent(KeyEvent event) {
         Log.i(TAG, "injectKeyEvent: " + event);
-        InputManager.getInstance().injectInputEvent(event,
+        InputManager input = InputManager.getInstance();
+        if (input != null) {
+            input.injectInputEvent(event,
                 InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH);
+        } else {
+            System.err.println("InputManager service is not available.");
+        }
     }
 
     /**
@@ -238,8 +243,13 @@ public class Input {
                 DEFAULT_EDGE_FLAGS);
         event.setSource(inputSource);
         Log.i("Input", "injectMotionEvent: " + event);
-        InputManager.getInstance().injectInputEvent(event,
+        InputManager input = InputManager.getInstance();
+        if (input != null) {
+            input.injectInputEvent(event,
                 InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH);
+        } else {
+            System.err.println("InputManager service is not available.");
+        }
     }
 
     private static final float lerp(float a, float b, float alpha) {
