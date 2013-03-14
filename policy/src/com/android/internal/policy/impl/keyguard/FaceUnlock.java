863223bcec571a8d96136a2ce7c9c866f1c766ba
Sun, Taiyi <taiyi.sun@intel.com>
2013-03-14 11:25:51 +0800

    AWARE: New Feature-Support Aware Voice Keyguard
    BZ: 103425
    
    1. Added a new config variable "lockscreen.biometric_weak_option" to
       indicate whether to use face unlock (0, default) or voice unlock (1)
    2. In LockPatternUtils.java, overloaded method saveLockPattern and method
       saveLockPassword to deliver one more parameter "option", and kept the
       original method behavior unchanged.
    3. In FaceUnlock.java, make a judgement on the option to decide whether
       to start face unlock keyguard or voice unlock keyguard.
    
    Change-Id: I179cd5b4f993c2ec57c47a685da4ca38400aaa17
    Signed-off-by: Sun, Taiyi <taiyi.sun@intel.com>
    Reviewed-on: http://android.intel.com:8080/104592
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Zheng, Huan <huan.zheng@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Tested-by: Zheng, Huan <huan.zheng@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- policy/src/com/android/internal/policy/impl/keyguard/FaceUnlock.java
+++ policy/src/com/android/internal/policy/impl/keyguard/FaceUnlock.java
@@ -128,7 +128,17 @@ public class FaceUnlock implements BiometricSensorUnlock, Handler.Callback {
         if (!mBoundToService) {
             Log.d(TAG, "Binding to Face Unlock service for user="
                     + mLockPatternUtils.getCurrentUser());
-            mContext.bindService(new Intent(IFaceLockInterface.class.getName()),
+            int option = mLockPatternUtils.getBiometricWeakOption();
+            String action = "";
+            if (option == LockPatternUtils.BIOMETRIC_WEAK_OPTION_FACE) {
+                action = IFaceLockInterface.class.getName();
+            } else if (option == LockPatternUtils.BIOMETRIC_WEAK_OPTION_VOICE) {
+                action = "aware.intent.action.VOICELOCK";
+            } else {
+                Log.e(TAG, "Unknown Biometric Weak Option: " + Integer.toString(option));
+                return false;
+            }
+            mContext.bindService(new Intent(action),
                     mConnection,
                     Context.BIND_AUTO_CREATE,
                     mLockPatternUtils.getCurrentUser());
