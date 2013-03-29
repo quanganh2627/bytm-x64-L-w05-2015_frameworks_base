df20c5330638296983f2ce7e67f5a4af384f4abc
Jean-Michel Bachot <jean-michel.bachot@intel.com>
2013-03-29 11:50:26 +0100

    [PORT FROM MAIN]widi : no scan request was launched after disconnection
    BZ: 84085
    
    Trigger a scan after WiFi display device has been disconnected.
    
    Category: device enablement
    Domain: CWS.WIFI-Common
    Origin: internal
    Upstream-Candidate: yes
    
    Change-Id: I00f9cba931880382d339fd90d61f7d61dc6ac879
    Orig-Change-Id: I37ced5a96fa358a2a6822679df1b6f1d0012349f
    Signed-off-by: Jean-Michel Bachot <jean-michel.bachot@intel.com>
    Reviewed-on: http://android.intel.com:8080/122974
    Reviewed-by: Naoumenko, PaulX <paulx.naoumenko@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Poulain, LoicX <loicx.poulain@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/com/android/server/display/WifiDisplayAdapter.java
+++ services/java/com/android/server/display/WifiDisplayAdapter.java
@@ -605,6 +605,8 @@ final class WifiDisplayAdapter extends DisplayAdapter {
                         || mActiveDisplay != null) {
                     mActiveDisplayState = WifiDisplayStatus.DISPLAY_STATE_NOT_CONNECTED;
                     mActiveDisplay = null;
+                    // Initiate a p2p scan after disconnection
+                    requestScanLocked();
                     scheduleStatusChangedBroadcastLocked();
                 }
             }
