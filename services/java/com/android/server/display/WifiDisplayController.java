65c66fde3b4b4a3abb094f6090f4b7f392eb1d71
Matthieu Mauger <matthieux.mauger@intel.com>
2013-07-03 16:58:12 +0200

    [PORT FROM MAIN] WiFi Display: Flush previous p2p scan results when enabling WFD feature
    BZ: 118015
    
    When WFD feature is OFF, p2p scan is done without including WFD IE in probe
    requests. Some adapters like PTV3000 doesn't include WFD IE in their probe
    responses if this field was not included in the request. Thus, they was not
    considered as display devices.
    
    When enabling the WFD feature, the devices list was not refreshed and adapters
    responding without WFD IE were not proposed in the list to the user.
    
    This patch flushes all previously discovered devices when enabling the WFD
    feature and trigs a new scan in order to make a new discovery using WFD IE in
    probe requests.
    
    Category: AOSP improvement
    Domain: WIFI-Core
    Severity: medium
    Origin: internal
    Upstream-Candidate: yes
    
    Change-Id: Ia547aac66c2ac9d66a4731933bc9b3a7cf8e52b3
    Orig-Change-Id: Ie3eb282419c2b47ed64d44e2dee7c2d70acac87a
    Signed-off-by: Matthieu Mauger <matthieux.mauger@intel.com>
    Reviewed-on: http://android.intel.com:8080/122954
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Reviewed-by: Naoumenko, PaulX <paulx.naoumenko@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/com/android/server/display/WifiDisplayController.java
+++ services/java/com/android/server/display/WifiDisplayController.java
@@ -111,6 +111,9 @@ final class WifiDisplayController implements DumpUtils.Dump {
     // True if there is a call to discoverPeers in progress.
     private boolean mDiscoverPeersInProgress;
 
+    // True if a call to discoverPeers is scheduled.
+    private boolean mDiscoverPeersScheduled;
+
     // Number of discover peers retries remaining.
     private int mDiscoverPeersRetriesLeft;
 
@@ -263,6 +266,7 @@ final class WifiDisplayController implements DumpUtils.Dump {
                             Slog.d(TAG, "Successfully set WFD info.");
                         }
                         if (mWfdEnabling) {
+                            restartDiscoverPeers();
                             mWfdEnabling = false;
                             mWfdEnabled = true;
                             reportFeatureState();
@@ -371,6 +375,10 @@ final class WifiDisplayController implements DumpUtils.Dump {
                 }
 
                 mDiscoverPeersInProgress = false;
+                if (mDiscoverPeersScheduled) {
+                    mDiscoverPeersScheduled = false;
+                    discoverPeers();
+                }
             }
 
             @Override
@@ -380,6 +388,11 @@ final class WifiDisplayController implements DumpUtils.Dump {
                 }
             }
         });
+    }
+
+    private void restartDiscoverPeers() {
+        mDiscoverPeersScheduled = true;
+        stopDiscoverPeers();
     }
 
     private void requestPeers() {
