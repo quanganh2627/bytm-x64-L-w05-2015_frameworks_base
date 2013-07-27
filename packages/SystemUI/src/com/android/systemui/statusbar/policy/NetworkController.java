fc351f69e7831dc7760869283f504e22cb11c10e
jwan <jia.wan@intel.com>
2013-07-28 01:15:55 +0800

    [PORT FROM MAIN] NetworkController: display the carrier name even when there is no data connection
    BZ: 81455
    
    The carrier name is replaced by 'NO INTERNET CONNECTION' when no
    data connection available. It was requested by CCB to change this
    behavior to have the carrier name displayed in all cases. This is
    requested for some operator.
    
    This patch changes the default behavior of the Android/UI to have
    the carrier name displayed even when there is no data connection.
    If the carrier name is empty the 'NO INTERNET CONNECTION' message
    is displayed.
    
    Domain: AOSP-Framework-SystemUI
    Category: aosp improvement
    Severity: medium
    Origin: Internal
    Upstream-Candidate: Yes
    Orig-Change-Id: I168117ad1b04e842af787f72828ac01ba465554c
    Change-Id: I55dfb66b82e67a9c4626423876aefb2c83ecca06
    Signed-off-by: Guillaume Lucas <guillaume.lucas@intel.com>
    Reviewed-on: http://android.intel.com:8080/96313
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Tested-by: Shen, JianchunX <jianchunx.shen@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -1033,12 +1033,12 @@ public class NetworkController extends BroadcastReceiver {
             mobileLabel = "";
         } else {
             // We want to show the carrier name if in service and either:
-            //   - We are connected to mobile data, or
+            //   - We are connected or not to mobile data, or
             //   - We are not connected to mobile data, as long as the *reason* packets are not
             //     being routed over that link is that we have better connectivity via wifi.
             // If data is disconnected for some other reason but wifi (or ethernet/bluetooth)
             // is connected, we show nothing.
-            // Otherwise (nothing connected) we show "No internet connection".
+            // If carrier name is empty and we are not conected we show "No internet connection".
 
             if (mDataConnected) {
                 mobileLabel = mNetworkName;
@@ -1051,8 +1051,12 @@ public class NetworkController extends BroadcastReceiver {
                     mobileLabel = "";
                 }
             } else {
-                mobileLabel
-                    = context.getString(R.string.status_bar_settings_signal_meter_disconnected);
+                if (mNetworkName != null && mNetworkName.length() != 0) {
+                    mobileLabel = mNetworkName;
+                } else {
+                    mobileLabel
+                        = context.getString(R.string.status_bar_settings_signal_meter_disconnected);
+                }
             }
 
             // Now for things that should only be shown when actually using mobile data.
