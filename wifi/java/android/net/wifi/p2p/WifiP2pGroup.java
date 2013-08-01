d39ccd43965d8f8b7bf45006b6e82d51b0fbc893
Paul Naoumenko <paulx.naoumenko@intel.com>
2013-08-01 20:18:04 +0200

    [PORT FROM MAIN] WiFi Display: Adapter reconnection upon AP just connected on another frequency
    BZ: 113453 119803
    
    Since Broadcom chip has some limitation to offer high video quality while used
    in muli-role and multi-channel, the best solution is to use the same channel as
    the access point when running in multi-role.
    
    This solution is already in place when DUT is first connected to an access point
    and then connected to a video adapter. However, in some case we can image that
    user first connects to video adapter and then connects to an access point. In
    such situation, there is a  high probability that adapter and access point
    doesn't run on the same channel.
    
    This patch detects when DUT is reaching an access point with a different
    frequency than video adapter, a dialog box informs the user that the video link
    will be closed and restored for better quality. The user has no other choice
    than accept and the patch manages the disconnection, retrieves the adapter we
    left and finally performs the reconnection.
    
    Fix Klocwork issues.
    
    Category: AOSP improvement
    Domain: WIFI-Core
    Severity: high
    Origin: internal
    Upstream-Candidate: no
    
    Change-Id: I0c7257fb72090cef80d643a0fe556790b0749252
    Orig-Change-Id: Id802e75938615db841387a44c129c520502a73bf
    Orig-Change-Id: Ib0144e03673fafbafb9a3a11b01fb7f38b5ad2ef
    Signed-off-by: Matthieu Mauger <matthieux.mauger@intel.com>
    Reviewed-on: http://android.intel.com:8080/122826
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Reviewed-by: Naoumenko, PaulX <paulx.naoumenko@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- wifi/java/android/net/wifi/p2p/WifiP2pGroup.java
+++ wifi/java/android/net/wifi/p2p/WifiP2pGroup.java
@@ -65,6 +65,9 @@ public class WifiP2pGroup implements Parcelable {
     /** The network id in the wpa_supplicant */
     private int mNetId;
 
+    /** The frequency */
+    private int mFrequency;
+
     /** P2P group started string pattern */
     private static final Pattern groupStartedPattern = Pattern.compile(
         "ssid=\"(.+)\" " +
@@ -114,7 +117,7 @@ public class WifiP2pGroup implements Parcelable {
 
             mNetworkName = match.group(1);
             //freq and psk are unused right now
-            //int freq = Integer.parseInt(match.group(2));
+            mFrequency = Integer.parseInt(match.group(2));
             //String psk = match.group(3);
             mPassphrase = match.group(4);
             mOwner = new WifiP2pDevice(match.group(5));
@@ -262,6 +265,16 @@ public class WifiP2pGroup implements Parcelable {
         this.mNetId = netId;
     }
 
+    /** @hide */
+    public int getFrequency() {
+        return mFrequency;
+    }
+
+    /** @hide */
+    public void setFrequency(int frequency) {
+        this.mFrequency = frequency;
+    }
+
     public String toString() {
         StringBuffer sbuf = new StringBuffer();
         sbuf.append("network: ").append(mNetworkName);
@@ -272,6 +285,7 @@ public class WifiP2pGroup implements Parcelable {
         }
         sbuf.append("\n interface: ").append(mInterface);
         sbuf.append("\n networkId: ").append(mNetId);
+        sbuf.append("\n frequency: ").append(mFrequency);
         return sbuf.toString();
     }
 
@@ -290,6 +304,7 @@ public class WifiP2pGroup implements Parcelable {
             mPassphrase = source.getPassphrase();
             mInterface = source.getInterface();
             mNetId = source.getNetworkId();
+            mFrequency = source.getFrequency();
         }
     }
 
@@ -305,6 +320,7 @@ public class WifiP2pGroup implements Parcelable {
         dest.writeString(mPassphrase);
         dest.writeString(mInterface);
         dest.writeInt(mNetId);
+        dest.writeInt(mFrequency);
     }
 
     /** Implement the Parcelable interface */
@@ -322,6 +338,7 @@ public class WifiP2pGroup implements Parcelable {
                 group.setPassphrase(in.readString());
                 group.setInterface(in.readString());
                 group.setNetworkId(in.readInt());
+                group.setFrequency(in.readInt());
                 return group;
             }
 
