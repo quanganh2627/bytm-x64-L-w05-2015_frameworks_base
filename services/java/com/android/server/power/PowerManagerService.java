63d0aa05a28d03c818aa342f5613e184ec7c1f5c
Delance <olivierx.delance@intel.com>
2013-08-01 12:01:17 +0200

    [PORT FROM MAIN] Add flags ACQUIRE_CAUSES_WAKEUP & ON_AFTER_RELEASE to wakelock log
    BZ: 101788
    
    These flags are useful for battery drain issues investigation.
    
    Change-Id: Ic50500a18eb2f242df6bef24db1c640b7e59bee1
    Orig-Change-Id: Ia032f971de7a30f5badaa89e759d44c519de60ea
    Signed-off-by: Delance <olivierx.delance@intel.com>
    Signed-off-by: Catalin Popescu <catalin.popescu@intel.com>
    Reviewed-on: http://android.intel.com:8080/122655
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -631,7 +631,8 @@ public final class PowerManagerService extends IPowerManager.Stub
             if (SystemProperties.get("wakelock.trace", "unknown").equals("1")) {
                 Slog.i("WAKELOCK_ACQUIRE", "TIMESTAMP=" + SystemClock.elapsedRealtimeNanos()
                         + ", TAG=" + tag + ", TYPE=" + wakeLock.getLockLevelString()
-                        + ", COUNT=0" + ", PID=" + pid + ", UID=" + uid);
+                        + ", COUNT=0" + ", PID=" + pid + ", UID=" + uid
+                        + ", FLAGS=" + wakeLock.getLockFlagsString());
             }
 
             applyWakeLockFlagsOnAcquireLocked(wakeLock);
@@ -707,7 +708,7 @@ public final class PowerManagerService extends IPowerManager.Stub
                 Slog.i("WAKELOCK_RELEASE", "TIMESTAMP=" + SystemClock.elapsedRealtimeNanos()
                         + ", TAG=" + wakeLock.mTag + ", TYPE=" + wakeLock.getLockLevelString()
                         + ", COUNT=0" + ", PID=" + wakeLock.mOwnerPid + ", UID="
-                        + wakeLock.mOwnerUid);
+                        + wakeLock.mOwnerUid + ", FLAGS=" + wakeLock.getLockFlagsString());
             }
         }
     }
@@ -736,7 +737,7 @@ public final class PowerManagerService extends IPowerManager.Stub
                 Slog.i("WAKELOCK_RELEASE", "TIMESTAMP=" + SystemClock.elapsedRealtimeNanos()
                         + ", TAG=" + wakeLock.mTag + ", TYPE=" + wakeLock.getLockLevelString()
                         + ", COUNT=0" + ", PID=" + wakeLock.mOwnerPid + ", UID="
-                        + wakeLock.mOwnerUid);
+                        + wakeLock.mOwnerUid + ", FLAGS=" + wakeLock.getLockFlagsString());
             }
         }
     }
