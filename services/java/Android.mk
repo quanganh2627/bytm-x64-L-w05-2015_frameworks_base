2f679b0005fde54f47e690b8c54a68d2c3479e45
Jean Trivelly <jean.trivelly@intel.com>
2013-08-08 16:23:13 +0200

    [PORT FROM MAIN JB-MR2]wifi: EAP-SIM/AKA integration with CSM
    BZ: 126071
    
    This patch makes EAP-SIM/AKA possible without PCSC-lite/SCAPI,
    Cws service Manager is used instead and provide direct
    access to telephony stack.
    
    Cws service manager is not registered to the system server,
    so it is no bindable from a native application (the supplicant).
    
    This patch adds a intermediate system service class that is just
    a passthrough to the cws service manager.
    This make the Cws services usable from a native application
    and so APU could be sent by the supplicant.
    
    Change-Id: I95f59493295950e5d878346148efdc6ab11c0ec6
    Orig-Change-Id: I84ca4f77e38c776d060063ccdc9fc270e0634fe6
    Category: feature differentiation
    Domain: CWS.WIFI-CSM
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Honore Tricot <honorex.tricot@intel.com>
    Signed-off-by: Jean Trivelly <jean.trivelly@intel.com>
    Reviewed-on: http://android.intel.com:8080/119930
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- services/java/Android.mk
+++ services/java/Android.mk
@@ -7,7 +7,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
             $(call all-subdir-java-files) \
 	    com/android/server/EventLogTags.logtags \
-	    com/android/server/am/EventLogTags.logtags
+	    com/android/server/am/EventLogTags.logtags \
+	    com/android/server/wifi/ICsmWifiOffloadSystemService.aidl
 
 LOCAL_STATIC_JAVA_LIBRARIES := CwsServiceMgr CsmClient
 
