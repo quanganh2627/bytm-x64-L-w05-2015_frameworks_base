f1d7c778a3336d332beb3f3bd3e31651537607a9
Matthieu Mauger <matthieux.mauger@intel.com>
2013-09-25 11:24:10 +0200

    [PORT FROM MAIN]wifi: Dynamically check if dual band is supported
    BZ: 135249
    
    Until now the dual band capability of a platform was hard stored in an XML file.
    This had some limitations by not taking into account the local regulatory.
    
    This patch gives a chance to check at runtime if dual band is really supported
    by using an existing supplicant command. However if the response to this command
    cannot be parsed the old method is still used.
    
    Change-Id: I991ff0ea0de46fc8aa76e864aa1acd9cdb26fe00
    Orig-Change-Id: I07f73c257610d8e152a18e92b203eeeba99bd72e
    Category: device enablement
    Domain: CWS.WIFI-Common
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Matthieu Mauger <matthieux.mauger@intel.com>
    Reviewed-on: http://android.intel.com:8080/135550
    Reviewed-by: Izoard, CedricX <cedricx.izoard@intel.com>
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: Peng, QingwenX <qingwenx.peng@intel.com>
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Lestienne, ChristopheX <christophex.lestienne@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -1574,6 +1574,22 @@ public class WifiStateMachine extends StateMachine {
     }
 
     /**
+     * Returns a list of supported frequency bands
+     */
+    public List<Integer> getSupportedBands() {
+        String capabilities = mWifiNative.getChannelsCapability();
+        if (capabilities == null) return null;
+        List<Integer> bandsList = new ArrayList<Integer>();
+        if (capabilities.contains("Mode[B]") || capabilities.contains("Mode[G]")) {
+            bandsList.add(WifiManager.WIFI_FREQUENCY_BAND_2GHZ);
+        }
+        if (capabilities.contains("Mode[A]")) {
+            bandsList.add(WifiManager.WIFI_FREQUENCY_BAND_5GHZ);
+        }
+        return bandsList;
+    }
+
+    /**
      * Returns the wifi configuration file
      */
     public String getConfigFile() {
