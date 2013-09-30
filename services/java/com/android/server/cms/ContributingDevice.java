0d9fbe95761be2b262f3c837bf6d4332f3e667b8
Saranya Gopal <saranya.gopal@intel.com>
2013-10-01 02:42:14 +0530

    CurrentMgmtService: Use XML configuration for device throttling
    BZ: 139166
    
    Currently, Current Management Service uses hard-coded values
    for throttling devices to avoid peak current and hence
    throttling is not configurable.
    
    Solution:This patch is to make CurrentMgmtService use XML
    configuration to decide upon the devices to throttle and
    throttle values corresponding to each state.
    Category: device enablement
    
    Domain: EM.Battery
    Origin: internal
    Upstream-Candidate: no
    
    Change-Id: I115412b607b7ef44eea1fbcae28f70194c65a065
    Signed-off-by: Saranya Gopal <saranya.gopal@intel.com>
    Reviewed-on: http://android.intel.com:8080/133434
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Tested-by: Koosalapur, Siddalingesh S <siddalingesh.s.koosalapur@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

---
+++ services/java/com/android/server/cms/ContributingDevice.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2012 Intel Corporation All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.server.cms;
+import java.util.ArrayList;
+
+public class ContributingDevice {
+    private int mDevID;
+    private String mName;
+    private ArrayList<throttleTrigger> mThrottleTriggers;
+    private int mTriggercnt = 0;
+
+    public ContributingDevice() {
+        mThrottleTriggers = new ArrayList<throttleTrigger>();
+    }
+
+    public void addThrottleTrigger(throttleTrigger tt) {
+        mThrottleTriggers.add(tt);
+    }
+
+    public throttleTrigger getThrottleTriggerByID(int id) {
+        return mThrottleTriggers.get(id);
+    }
+
+    public class throttleTrigger {
+
+        private String mName;
+        private int mStateCnt = 0;
+        private ArrayList<Integer> mStateIDs;
+        private ArrayList<Integer> mThrottleValues;
+        private int mTriggerid;
+
+        public throttleTrigger() {
+            this.mTriggerid = mTriggercnt++;
+            mStateIDs = new ArrayList<Integer>();
+            mThrottleValues = new ArrayList<Integer>();
+        }
+
+        public void addStateID() {
+            mStateIDs.add(mStateCnt++);
+        }
+
+        public void addThrottleValue(int value) {
+            mThrottleValues.add(value);
+        }
+
+        public int getThrottleValue(int triggerid, int stateid) {
+            return mThrottleTriggers.get(triggerid).mThrottleValues.get(stateid);
+        }
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public void setID(int id) {
+        this.mDevID = id;
+    }
+
+    public int getID() {
+        return this.mDevID;
+    }
+}
