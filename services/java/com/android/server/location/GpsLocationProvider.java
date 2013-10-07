d98e293c91c09dba94e756db8b5cbb3af915566f
Frederic Berat <fredericx.berat@intel.com>
2013-10-07 17:18:55 +0200

    [PORT FROM MAIN][GPS] Calls getPhoneType with network mode
    BZ: 142826
    
    On some boards, isVoiceCapable returns false which leads to getPhoneType
    to return PHONE_TYPE_NONE which is not expected by GNSS framework.
    By changing the call by adding the network mode by-pass this defect by
    getting the static phone type.
    
    Change-Id: I7e18a0fc57ce9f9d5b36252d850c763d8fe8a3f5
    Orig-Change-Id: I2f4b5ec5597d50d71cc62b22b11b1297c35fc388
    Category: device enablement
    Domain: CWS.GPS-Tablets
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Frederic Berat <fredericx.berat@intel.com>
    Signed-off-by: jerome Pantaloni <jeromex.pantaloni@intel.com>

--- services/java/com/android/server/location/GpsLocationProvider.java
+++ services/java/com/android/server/location/GpsLocationProvider.java
@@ -79,6 +79,7 @@ import com.android.internal.location.ProviderRequest;
 import com.android.internal.location.GpsNetInitiatedHandler.GpsNiNotification;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
+import com.android.internal.telephony.RILConstants;
 
 import java.io.File;
 import java.io.FileDescriptor;
@@ -560,10 +561,15 @@ public class GpsLocationProvider implements LocationProviderInterface {
                 mRefLocHandler = new Handler() {
                     public void handleMessage(Message msg) {
                         int flags = (int)msg.what;
+                        int preferredNetworkMode = RILConstants.PREFERRED_NETWORK_MODE;
+                        int networkMode = Settings.Global.getInt(mContext.getContentResolver(),
+                                Settings.Global.PREFERRED_NETWORK_MODE, preferredNetworkMode);
+
+                        int phoneType = mTelephonyManager.getPhoneType(networkMode);
 
                         try {
                             if ((flags & AGPS_REQUEST_REFLOC_CELLID) == AGPS_REQUEST_REFLOC_CELLID) {
-                                if (mTelephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM) {
+                                if (phoneType == TelephonyManager.PHONE_TYPE_GSM) {
                                     mRefLocationRequested = true;
                                     CellLocation.requestLocationUpdate();
                                     synchronized(mCellLocationLock) {
