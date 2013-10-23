fe149eab773bbf09afe21fad2a024764c27e5197
Han, He <he.han@intel.com>
2013-10-23 21:52:47 +0800

    [PORT FROM MAIN] Enable Terminal sensor to optimize WindowRotation.
    BZ: 147019
    
    Enable Terminal sensor to optimize WindowRotation.
    
    Category: device enablement or aosp improvements
    Domain: SENSORS.ACCEL-Common
    Origin: Internal
    Upstream-Candidate: no, proprietary
    
    Change-Id: Idb30a546b106e28afebde5cdf658fc28b7187fe6
    Orig-Change-Id: Ied4559f33df9807b5fd1444c589a7674c880c5c7
    Signed-off-by: Han, He <he.han@intel.com>

--- policy/src/com/android/internal/policy/impl/WindowOrientationListener.java
+++ policy/src/com/android/internal/policy/impl/WindowOrientationListener.java
@@ -58,6 +58,15 @@ public abstract class WindowOrientationListener {
 
     private final Object mLock = new Object();
 
+    /* Using Terminal Sensor to detect rotation first */
+    private boolean mEnabledTerminal = false;
+    private final int SENSOR_TYPE_TERMINAL = 103;
+    private final int ACCEL_EVENT_MAX = 60;
+    private Sensor mTerminal;
+    private TerminalEventListenerImpl mTerminalEventListener;
+    private int mAccelEventCount = 0;
+    private boolean mAccelRegistered = false;
+
     /**
      * Creates a new WindowOrientationListener.
      * 
@@ -81,6 +90,9 @@ public abstract class WindowOrientationListener {
      * This constructor is private since no one uses it.
      */
     private WindowOrientationListener(Context context, Handler handler, int rate) {
+        mEnabledTerminal = context.getResources().getBoolean(
+            com.android.internal.R.bool.config_TerminalDetectWindowOrientation);
+
         mHandler = handler;
         mSensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
         mRate = rate;
@@ -89,6 +101,15 @@ public abstract class WindowOrientationListener {
         if (mSensor != null) {
             // Create listener only if sensors do exist
             mSensorEventListener = new SensorEventListenerImpl();
+        }
+
+        if (mEnabledTerminal) {
+            mTerminal = mSensorManager.getDefaultSensor(SENSOR_TYPE_TERMINAL);
+            if (mTerminal != null) {
+                mTerminalEventListener = new TerminalEventListenerImpl();
+            } else {
+                mEnabledTerminal = false;
+            }
         }
     }
 
@@ -107,7 +128,11 @@ public abstract class WindowOrientationListener {
                     Log.d(TAG, "WindowOrientationListener enabled");
                 }
                 mSensorEventListener.resetLocked();
-                mSensorManager.registerListener(mSensorEventListener, mSensor, mRate, mHandler);
+                if (mEnabledTerminal)
+                    mSensorManager.registerListener(mTerminalEventListener, mTerminal,
+                                                    SensorManager.SENSOR_DELAY_NORMAL, mHandler);
+                else
+                    mSensorManager.registerListener(mSensorEventListener, mSensor, mRate, mHandler);
                 mEnabled = true;
             }
         }
@@ -126,7 +151,14 @@ public abstract class WindowOrientationListener {
                 if (LOG) {
                     Log.d(TAG, "WindowOrientationListener disabled");
                 }
-                mSensorManager.unregisterListener(mSensorEventListener);
+                if (mEnabledTerminal) {
+                    mSensorManager.unregisterListener(mTerminalEventListener);
+                    if (mAccelRegistered) {
+                        mSensorManager.unregisterListener(mSensorEventListener);
+                        mAccelRegistered = false;
+                    }
+                } else
+                    mSensorManager.unregisterListener(mSensorEventListener);
                 mEnabled = false;
             }
         }
@@ -180,6 +212,19 @@ public abstract class WindowOrientationListener {
      * @see android.view.Surface
      */
     public abstract void onProposedRotationChanged(int rotation);
+
+    private class RotationDone extends Thread {
+        @Override
+        public void run() {
+            synchronized (mLock) {
+                if (mAccelRegistered) {
+                    mSensorManager.unregisterListener(mSensorEventListener);
+                    mAccelRegistered = false;
+                }
+                mAccelEventCount = 0;
+            }
+        }
+    }
 
     /**
      * This class filters the raw accelerometer data and tries to detect actual changes in
@@ -562,6 +607,14 @@ public abstract class WindowOrientationListener {
                 }
                 onProposedRotationChanged(proposedRotation);
             }
+
+            if (mEnabledTerminal) {
+                mAccelEventCount++;
+                if ((proposedRotation != oldProposedRotation && proposedRotation >= 0) ||
+                    mAccelEventCount >= ACCEL_EVENT_MAX) {
+                    new RotationDone().start();
+                }
+            }
         }
 
         /**
@@ -728,6 +781,25 @@ public abstract class WindowOrientationListener {
 
         private float remainingMS(long now, long until) {
             return now >= until ? 0 : (until - now) * 0.000001f;
+        }
+    }
+
+    final class TerminalEventListenerImpl implements SensorEventListener {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            synchronized (mLock) {
+                if (!mAccelRegistered) {
+                    mSensorManager.registerListener(mSensorEventListener, mSensor,
+                                                    SensorManager.SENSOR_DELAY_FASTEST,
+                                                    mHandler);
+                    mAccelRegistered = true;
+                }
+                mAccelEventCount = 0;
+            }
         }
     }
 }
