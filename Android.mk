96dbfb19c19ba483a7903542f5fd19773ba45466
Jeevaka Badrappan <jeevaka.badrappan@intel.com>
2013-11-06 19:56:58 +0200

    [PORT FROM MAIN] Telephony: Add IOemTelephony and Oem specific constants
    BZ: 25540
    
    Currently, IOemTelephony.aidl resides
    inside the device/intel/apps/OemTelephony folder.
    Due to this, applications and frameworks are
    not able to import this.
    
    There is a requirement for IOemTelephony
    to be used in both framework and applications.
    
    With this patch, IOemTelephony interface file and
    also OEM specific constants are moved from device
    specific folders to the frameworks folder.
    
    Severity: medium
    
    Change-Id: I601247b211027d70d4b97b873b263f77fc0bb649
    Orig-Change-Id: Ia7bbab554467eba96017d35791b924b9d83ea82e
    Category: device enablement
    Domain: Telephony-Base
    Origin: internal
    Upstream-Candidate: no,proprietary
    Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Arun Ravindran <arun.ravindran@intel.com>

--- Android.mk
+++ Android.mk
@@ -267,6 +267,7 @@ LOCAL_SRC_FILES += \
 	telephony/java/com/android/internal/telephony/ISms.aidl \
 	telephony/java/com/android/internal/telephony/ITelephonyRegistry.aidl \
 	telephony/java/com/android/internal/telephony/IWapPushManager.aidl \
+        telephony/java/com/intel/internal/telephony/OemTelephony/IOemTelephony.aidl \
 	wifi/java/android/net/wifi/IWifiManager.aidl \
 	wifi/java/android/net/wifi/p2p/IWifiP2pManager.aidl \
 	packages/services/PacProcessor/com/android/net/IProxyService.aidl \
