9f70a119a468963b252d3ea07aa9ec7829f4002e
Lu, Shenghua <shenghua.lu@intel.com>
2013-11-14 16:18:43 +0800

    [PORT FROM MAIN] libhwui: Temply enable the Scissor test for textureView rotationX/Y
    BZ: 143889
    
    This issue is due to the calculation ignorees the possibility that
    transformed the coordinate lie behind the camera when rotateX/Y, and caused the vertex
    coordinate outside the hardwarelayer clip rect. I prefer there's something
    wrong with the transform calculate, but it's hard to find out the reason,
    So currently I had to enable Scissor test for such cases, once I find out the
    transformation issue, I would like to re-enable the optimization for scissor-testing.
    
    Orig-Change-Id: I4ee35a7153f72366dd7e12e027c0d6601028bbec
    Category: aosp improvement
    Domain: AOSP-Framework-GUI
    Origin: internal
    Upstream-Candidate: no
    Change-Id: I54d5d9d9f5afcf3b3c08e22eb6c047ce25d496b1
    Signed-off-by: Shenghua Lu <shenghua.lu@intel.com>

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -3081,7 +3081,7 @@ status_t OpenGLRenderer::drawLayer(Layer* layer, float x, float y) {
 
     updateLayer(layer, true);
 
-    mCaches.setScissorEnabled(mScissorOptimizationDisabled || clipRequired);
+    mCaches.setScissorEnabled(true);
     mCaches.activeTexture(0);
 
     if (CC_LIKELY(!layer->region.isEmpty())) {
