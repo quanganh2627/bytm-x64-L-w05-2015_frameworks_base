828887cacde7aa0f184d8d540e046f5f98e9ff17
Norvin Shuster <norvin.shuster@intel.com>
2013-12-09 11:43:45 -0800

    ASF: Remove external references to ASF
    BZ: 154336
    
    Remove the external references to the AsfAosp.java in the build.  The
    ASF library (com.intel.asf) will include the disabled hooks or the
    enabled hooks instead of the frameworks build building the
    enable/disable AsfAosp.java directly.
    
    Change-Id: Ie2376902e500a25a3b3eae1e57c32e50a69613d4
    Category: feature differentiation
    Domain: AOSP.Security-ASF
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Norvin Shuster <norvin.shuster@intel.com>

--- services/java/Android.mk
+++ services/java/Android.mk
@@ -22,14 +22,8 @@ LOCAL_MODULE:= services
 
 LOCAL_JAVA_LIBRARIES := android.policy conscrypt telephony-common com.intel.multidisplay com.intel.config
 
-ifeq ($(strip $(INTEL_FEATURE_ASF)),true)
-	LOCAL_SRC_FILES += $(call all-java-files-under, \
-		../../../../vendor/intel/asf/platform/enabled)
-	LOCAL_JAVA_LIBRARIES += com.intel.asf
-else
-	LOCAL_SRC_FILES += $(call all-java-files-under, \
-		../../../../vendor/intel/asf/platform/disabled)
-endif
+LOCAL_JAVA_LIBRARIES += com.intel.asf
+
 
 include $(BUILD_JAVA_LIBRARY)
 
