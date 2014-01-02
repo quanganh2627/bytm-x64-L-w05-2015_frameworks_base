5d67ec4824360628d29e72b5b88c23a96d206105
Du, Changbin <changbinx.du@intel.com>
2014-01-02 14:25:56 +0800

    avoid double call to DiscardData() function
    BZ: 162121
    
    If ReadJpegFile() fails, it will call DiscardData() internal. So we
    cannot call it again on this case.
    
    Change-Id: I8b340f485052c86874081a4a1ef39e425cd3b81d
    Category: aosp improvement
    Domain: USB-Mtp-ptp
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Du, Changbin <changbinx.du@intel.com>

--- media/jni/android_mtp_MtpDatabase.cpp
+++ media/jni/android_mtp_MtpDatabase.cpp
@@ -813,8 +813,8 @@ MtpResponseCode MyMtpDatabase::getObjectInfo(MtpObjectHandle handle,
                 info.mImagePixWidth = ImageInfo.Width;
                 info.mImagePixHeight = ImageInfo.Height;
             }
+            DiscardData();
         }
-        DiscardData();
     }
 
     checkAndClearExceptionFromCallback(env, __FUNCTION__);
