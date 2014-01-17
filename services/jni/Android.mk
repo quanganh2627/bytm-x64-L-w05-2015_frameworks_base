8339dfeef784f640177a743bc084e545556ba67e
Gongora, Enrique <enrique.gongora@intel.com>
2014-01-17 02:47:28 -0800

    Revert "ASF 2.0: Update of device control hook points for GPS device and screen capture."
    BZ: 165217
    
    This reverts commit 5f009a698f57f475570b3f109488e96d979baa1d.
    This revert patch https://android.intel.com/#/c/152550/
    
    Change-Id: Ic73b2c1e5b02f33ca3c6efc9f9841dcc5cb37a5b
    Category: feature differentiation
    Domain: AOSP.Security-ASF
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Enrique Gongora <enrique.gongora@intel.com>

--- services/jni/Android.mk
+++ services/jni/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 ifeq ($(strip $(INTEL_FEATURE_ASF)),true)
-    LOCAL_CPPFLAGS += -DINTEL_FEATURE_ASF
     LOCAL_CPPFLAGS += -DPLATFORM_ASF_VERSION=$(PLATFORM_ASF_VERSION)
 else
     LOCAL_CPPFLAGS += -DPLATFORM_ASF_VERSION=0
@@ -62,7 +61,6 @@ LOCAL_SHARED_LIBRARIES := \
     libGLESv2
 
 ifeq ($(strip $(INTEL_FEATURE_ASF)),true)
-    LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/asfaosp
 ifneq ($(strip $(PLATFORM_ASF_VERSION)),1)
 ifneq ($(strip $(PLATFORM_ASF_VERSION)),0)
     LOCAL_SHARED_LIBRARIES += libsecuritydeviceserviceclient
