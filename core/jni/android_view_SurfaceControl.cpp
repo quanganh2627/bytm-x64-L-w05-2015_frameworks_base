ceafb02b78499fa0f49f2565069c10f26796ce9e
Norvin Shuster <norvin.shuster@intel.com>
2014-01-17 21:41:48 -0800

    Revert "Revert "ASF 2.0: Update of device control hook points for GPS device and screen capture.""
    BZ: 165585
    
    This reverts commit 8339dfeef784f640177a743bc084e545556ba67e.
    This reverts patch https://android.intel.com/#/c/156805/
    
    Change-Id: I3b980fed6f7adbce252966730a38c2eea1e177df
    Category: feature differentiation
    Domain: AOSP.Security-ASF
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Norvin Shuster <norvin.shuster@intel.com>

--- core/jni/android_view_SurfaceControl.cpp
+++ core/jni/android_view_SurfaceControl.cpp
@@ -41,10 +41,13 @@
 
 #include <ScopedUtfChars.h>
 
-#if PLATFORM_ASF_VERSION >= 2
+#ifdef INTEL_FEATURE_ASF
+#include "AsfVersionAosp.h"
+#if PLATFORM_ASF_VERSION >= ASF_VERSION_2
 // The interface file for inserting hooks to communicate with native service securitydevice
 #include "AsfDeviceAosp.h"
 #include <private/android_filesystem_config.h>
+#endif
 #endif
 
 
@@ -123,7 +126,7 @@ static void nativeDestroy(JNIEnv* env, jclass clazz, jint nativeObject) {
     ctrl->decStrong((void *)nativeCreate);
 }
 
-#if PLATFORM_ASF_VERSION >= 2
+#if defined(INTEL_FEATURE_ASF) && (PLATFORM_ASF_VERSION >= ASF_VERSION_2)
 static bool notifyScreenCaptureAccess() {
     // Adding hook to call security device service
     const int pid = IPCThreadState::self()->getCallingPid();
@@ -150,7 +153,7 @@ static bool notifyScreenCaptureAccess() {
 static jobject nativeScreenshotBitmap(JNIEnv* env, jclass clazz, jobject displayTokenObj,
         jint width, jint height, jint minLayer, jint maxLayer, bool allLayers) {
 
-#if PLATFORM_ASF_VERSION >= 2
+#if defined(INTEL_FEATURE_ASF) && (PLATFORM_ASF_VERSION >= ASF_VERSION_2)
     // Place call to function that acts as a hook point for camera events
     bool response = notifyScreenCaptureAccess();
     // If response is false, deny access to requested application and return NULL.
