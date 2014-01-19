9455c4fc1e44a4dc6b24359136b5dfb99ec90161
yuanwu <yuan.wu@intel.com>
2014-01-20 00:12:54 +0800

    LPAL: enable the VtsvService in SystemServer
    BZ:166384
    
    :enable the VtsvService in SystemServer
    Change direcoty - framework/base/services/java/
                    - framework/base/core/java
    
    Change-Id: Ie34544bc593603175816044be67c6a55f47adeda
    Category: aosp improvement
    Domain: AOSP-Framework-CoreService
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: yuanwu <yuan.wu@intel.com>
    Signed-off-by: Matrix Yao <matrix.yao@intel.com>
    Signed-off-by: Jian, Qinghui <qinghui.jian@intel.com>

--- packages/Keyguard/Android.mk
+++ packages/Keyguard/Android.mk
@@ -29,7 +29,6 @@ LOCAL_JAVA_LIBRARIES := services com.intel.config
 
 ifeq ($(strip $(INTEL_FEATURE_LPAL)),true)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../vendor/intel/lpal/frameworks/enabled/base/packages/Keyguard/src)
-LOCAL_STATIC_JAVA_LIBRARIES := vtsvmanager
 else
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../vendor/intel/lpal/frameworks/disabled/base/packages/Keyguard/src)
 endif
