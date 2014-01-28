5e10f50bdbcdb22321cedc262f90ef5ddd0952d2
Jeevaka Badrappan <jeevaka.badrappan@intel.com>
2014-01-28 18:59:41 +0200

    Revert "Telephony: SMS application not available for no-voice devices"
    BZ: 156441
    
    Patch is reverted as the Android CTS test
    android.app.cts.SystemFeaturesTest#testTelephonyFeatures
    fails.
    
    This reverts the patch
    https://android.intel.com/#/c/146550/
    
    Change-Id: I05fd7ac4f7797db7fc220b7f130a438a956758c0
    Category: aosp improvement
    Domain: Telephony-Base
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -427,7 +427,7 @@ public class TelephonyManager {
      * @see #PHONE_TYPE_SIP
      */
     public int getPhoneType() {
-        if (!isVoiceCapable() && !isSmsCapable()) {
+        if (!isVoiceCapable()) {
             return PHONE_TYPE_NONE;
         }
         return getCurrentPhoneType();
