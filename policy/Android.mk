60412aa4e1cd5143be5e22354478dc42bd347b7f
Teng Yiliang <yiliang.teng@intel.com>
2014-02-26 21:53:33 +0800

    ARKHAM:fix build error when change Android.mk under serivce & policy.
    BZ: 176252
    
    To generate arkham prebuilt jar for external build,
    service/java/Android.mk & policy/Android.mk will be cross-referenced by
    arkham makefiles under vendor/.../PRIVATE folder, such dependency will
    cause build error if these two files are chanaged without updating
    arkham ones.  Generate a dedicated java lib for arkham to remove such
    dependency.
    
    Change-Id: I3ffa9937519f3904c6350c912e1c0195b521d1c8
    Category: feature differentiation
    Domain: AOSP.Security-Arkham
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Teng Yiliang <yiliang.teng@intel.com>

--- policy/Android.mk
+++ policy/Android.mk
@@ -20,10 +20,27 @@ LOCAL_ARKHAM_PATH := vendor/intel/arkham
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../$(LOCAL_ARKHAM_PATH)/frameworks/disabled/base/policy/src/)
 endif
 LOCAL_JAVA_LIBRARIES := com.intel.config
-            
+
 LOCAL_MODULE := android.policy
 
 include $(BUILD_JAVA_LIBRARY)
+
+# ============================================================
+#generate static java lib for arkham external build.
+ifeq ($(strip $(INTEL_FEATURE_ARKHAM)),true)
+ifeq ($(LOCAL_ARKHAM_PATH)/frameworks/enabled/base/policy/src, $(wildcard $(LOCAL_ARKHAM_PATH)/frameworks/enabled/base/policy/src))
+backup_list:= LOCAL_SRC_FILES LOCAL_JAVA_LIBRARIES LOCAL_STATIC_JAVA_LIBRARIES
+$(foreach item, $(backup_list), $(eval backup_$(item):=$($(item))))
+
+include $(CLEAR_VARS)
+$(foreach item, $(backup_list), $(eval $(item):=$(backup_$(item))))
+
+LOCAL_MODULE:=android.policy_static
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+droid: $(LOCAL_INTERMEDIATE_TARGETS)
+endif
+endif
 
 # additionally, build unit tests in a separate .apk
 include $(call all-makefiles-under,$(LOCAL_PATH))
--- services/java/Android.mk
+++ services/java/Android.mk
@@ -37,3 +37,21 @@ LOCAL_JAVA_LIBRARIES += com.intel.asf
 include $(BUILD_JAVA_LIBRARY)
 
 include $(BUILD_DROIDDOC)
+
+# ============================================================
+#generate static java lib for arkham external build.
+ifeq ($(strip $(INTEL_FEATURE_ARKHAM)),true)
+ifeq ($(LOCAL_ARKHAM_PATH)/frameworks/enabled/base/services, $(wildcard $(LOCAL_ARKHAM_PATH)/frameworks/enabled/base/services))
+
+backup_list:= LOCAL_SRC_FILES LOCAL_JAVA_LIBRARIES LOCAL_STATIC_JAVA_LIBRARIES
+$(foreach item, $(backup_list), $(eval backup_$(item):=$($(item))))
+
+include $(CLEAR_VARS)
+$(foreach item, $(backup_list), $(eval $(item):=$(backup_$(item))))
+
+LOCAL_MODULE:=services_static
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+droid: $(LOCAL_INTERMEDIATE_TARGETS)
+endif
+endif
