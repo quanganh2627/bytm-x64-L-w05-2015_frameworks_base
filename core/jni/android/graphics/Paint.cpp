38ccfac7b71f60896708b732583cb86d6fd918a2
Victoria Lease <violets@google.com>
2014-03-17 16:03:28 -0700

    [PORT FROM GOOGLE] malloc some advances
    BZ: 183454
    
    google bug id 13506939
    google change id I87ad616960c4f16bc55d8b906049dbd4f001d6a5
    
    Change-Id: I8ce2fe83445ca9ecf12d56d3d97c15b0dfa12109
    Category: aosp improvement
    Domain: AOSP-Framework-CoreService
    Origin: 3rd-party Google
    Upstream-Candidate: no, proprietary
    Signed-off-by: Gao,KunX <kunx.gao@intel.com>

--- core/jni/android/graphics/Paint.cpp
+++ core/jni/android/graphics/Paint.cpp
@@ -553,7 +553,7 @@ public:
                 return 0;
             }
         }
-        jfloat advancesArray[count];
+        jfloat* advancesArray = new jfloat[count];
         jfloat totalAdvance = 0;
 
         TextLayout::getTextRunAdvances(paint, text, start, count, contextCount, flags,
@@ -562,6 +562,7 @@ public:
         if (advances != NULL) {
             env->SetFloatArrayRegion(advances, advancesIndex, count, advancesArray);
         }
+        delete [] advancesArray;
         return totalAdvance;
     }
 
