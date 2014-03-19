93fd3b70bd2e655957f74d06f1eca450375a9e27
lwan89x <liangx.wang@intel.com>
2014-03-19 16:39:53 +0800

    [REVERT ME] Debug: Dump out TRIM_MEMORY process' time usage.
    BZ: 184169
    
    Change-Id: I8a566122d80def5c10d6c4a6afdb593180a5047e
    Category: aosp improvement
    Domain: AOSP-Framework-CoreService
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: lwan89x <liangx.wang@intel.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -4085,10 +4085,12 @@ public final class ActivityThread {
     }
 
     final void handleTrimMemory(int level) {
-        if (DEBUG_MEMORY_TRIM) Slog.v(TAG, "Trimming memory to level: " + level);
+        Slog.v(TAG, "**BZ184169** Trimming memory to level: " + level);
 
         final WindowManagerGlobal windowManager = WindowManagerGlobal.getInstance();
+        Slog.v(TAG, "**BZ184169**  windowManager.startTrimMemory");
         windowManager.startTrimMemory(level);
+        Slog.v(TAG, "**BZ184169**  onTrimMemory");
 
         ArrayList<ComponentCallbacks2> callbacks = collectComponentCallbacks(true, null);
 
@@ -4096,8 +4098,10 @@ public final class ActivityThread {
         for (int i = 0; i < N; i++) {
             callbacks.get(i).onTrimMemory(level);
         }
+        Slog.v(TAG, "**BZ184169**  windowManager.endTrimMemory()");
 
         windowManager.endTrimMemory();
+        Slog.v(TAG, "**BZ184169**  handleTrimMemory complete");
     }
 
     private void setupGraphicsSupport(LoadedApk info, File cacheDir) {
