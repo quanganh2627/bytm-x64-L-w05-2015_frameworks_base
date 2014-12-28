05e5af061b32b2f2618171443aed4c1f7f494011
CHEN Gang <gang.c.chen@intel.com>
2014-12-28 23:19:19 +0800

    ZygoteInit: Remove the frequent gc while preloading
    BZ: 119
    
    There're thousands of classes and resources will be preloaded
    in ZygoteInit while booting. We have no need to call gc in
    each preloading a class/resource.
    
    Remove the gc, we'll reduce around 1.6s booting up time.
    
            Default    Remove GC
    	51.811411	48.439615
    	49.407533	48.191704
    	49.628261	50.299479
    	50.722268	49.466105
    	50.389053	48.401547
    	51.210139	50.312334
    	51.346852	48.797163
    	51.849278	49.208611
    	49.871149	49.764517
    	51.916841	49.535035
    avg	50.8152785	49.241611
    
    Change-Id: I3fdc74ced6d23bcb94f96af4b9116f2a3db8b553
    Signed-off-by: CHEN Gang <gang.c.chen@intel.com>

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -306,15 +306,6 @@ public class ZygoteInit {
                             Log.v(TAG, "Preloading " + line + "...");
                         }
                         Class.forName(line);
-                        if (Debug.getGlobalAllocSize() > PRELOAD_GC_THRESHOLD) {
-                            if (false) {
-                                Log.v(TAG,
-                                    " GC at " + Debug.getGlobalAllocSize());
-                            }
-                            System.gc();
-                            runtime.runFinalizationSync();
-                            Debug.resetGlobalAllocSize();
-                        }
                         count++;
                     } catch (ClassNotFoundException e) {
                         Log.w(TAG, "Class not found for preloading: " + line);
@@ -399,14 +390,6 @@ public class ZygoteInit {
     private static int preloadColorStateLists(VMRuntime runtime, TypedArray ar) {
         int N = ar.length();
         for (int i=0; i<N; i++) {
-            if (Debug.getGlobalAllocSize() > PRELOAD_GC_THRESHOLD) {
-                if (false) {
-                    Log.v(TAG, " GC at " + Debug.getGlobalAllocSize());
-                }
-                System.gc();
-                runtime.runFinalizationSync();
-                Debug.resetGlobalAllocSize();
-            }
             int id = ar.getResourceId(i, 0);
             if (false) {
                 Log.v(TAG, "Preloading resource #" + Integer.toHexString(id));
@@ -427,14 +410,6 @@ public class ZygoteInit {
     private static int preloadDrawables(VMRuntime runtime, TypedArray ar) {
         int N = ar.length();
         for (int i=0; i<N; i++) {
-            if (Debug.getGlobalAllocSize() > PRELOAD_GC_THRESHOLD) {
-                if (false) {
-                    Log.v(TAG, " GC at " + Debug.getGlobalAllocSize());
-                }
-                System.gc();
-                runtime.runFinalizationSync();
-                Debug.resetGlobalAllocSize();
-            }
             int id = ar.getResourceId(i, 0);
             if (false) {
                 Log.v(TAG, "Preloading resource #" + Integer.toHexString(id));
